c256c81098ac45162b7a403eb51eddac0c4fd9ae
hassibur rahman <hassiburrahman@hassiburs-Mac-mini.local>
2026-07-05 18:55:10 +0600

    working on car detils page

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyCarData } from '../assets/assets'
 
 const CarDetails = () => {
-  return (
-    <div>
+
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const [car, setCar] = useState(null)
+
+  useEffect(()=>{
+    setCar(dummyCarData.fill(car => car._id === id))
+  },[id])
+
+  return car ? (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
 
     </div>
-  )
+  ) : <p>Loadig...</p>
 }
 
 export default CarDetails
